47b9e2ca6440f72158bfd9c81d7afa696a447841
MJS0942 <mohjjang3@gmail.com>
2023-11-25 20:52:04 +0900

    part of skeleton for question

--- question.cc
+++ question.cc
@@ -1,1 +1,28 @@
 #include "question.h"
+
+question::question (int score, string::string cat) : score(score), cat(cat)
+{
+    //get question about category from API
+    //set difficulty by score
+
+    return;
+}
+
+int question::check (string::string ans)
+{
+    //check if the answer is yes
+
+    return 1;
+}
+
+void question::printQ (int num)
+{
+    std::cout << num << ". " << Q << endl;
+
+    return;
+}
+
+int question::getscore ()
+{
+    return score;
+}
--- question.h
+++ question.h
@@ -1,1 +1,15 @@
+class question
+{
+    public:
+        question(int score, string::string cat);        //determine Q
+        
+        int check(string::string ans);        //return whether the answer is correct
 
+        void printQ(int num) const;
+        int getscore() const;
+
+    private:
+        string::string Q;
+        string::string cat;
+        int score;
+}
