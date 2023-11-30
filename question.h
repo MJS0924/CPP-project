5b255d9c4cffe38b60d70fc7b931c2fa3de84d45
Honak <khh874@naver.com>
2023-12-01 00:59:45 +0900

    update for player&question

--- question.h
+++ question.h
@@ -1,15 +1,37 @@
-class question
-{
-    public:
-        question(int score, string::string cat);        //determine Q
-        
-        int check(string::string ans);        //return whether the answer is correct
+#ifndef QUESTION_H
+#define QUESTION_H
 
-        void printQ(int num) const;
-        int getscore() const;
+#include <string>
 
-    private:
-        string::string Q;
-        string::string cat;
-        int score;
-}
+class Question {
+public:
+    Question(const std::string& body, const std::string& answer, int score);
+    ~Question();
+
+    virtual void display() const;
+    virtual const std::string& getAnswer() const;
+    virtual bool determine(const std::string& userAnswer) const;
+
+private:
+    std::string body;
+    std::string answer;
+    int score;
+};
+
+class QuestionWithFakeAnswer : public Question {
+public:
+    QuestionWithFakeAnswer(const std::string& body, const std::string& answer, int score, const std::string& fakeAnswer);
+    ~QuestionWithFakeAnswer();
+
+    void display() const override;
+    const std::string& getAnswer() const override;
+    bool determine(const std::string& userAnswer) const override;
+
+    void displayReal() const override;
+    const std::string& getRealAnswer() const override;
+    bool determineReal(const std::string& userAnswer) const override;
+private:
+    std::string fakeAnswer;
+};
+
+#endif // QUESTION_H
